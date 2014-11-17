cfdf0382c3f11a56198c33800ee46b966229d489
Simone Dalla <simodalla@gmail.com>
2014-11-17 12:31:05 +0100

    add applications configuration, view https://docs.djangoproject.com/en/1.7/ref/applications/

---
+++ pympa_core/apps.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, absolute_import
+
+from django.apps import AppConfig
+
+
+class PympaCoreConfig(AppConfig):
+    name = 'pympa_core'
+    verbose_name = 'Pympa Oggetti Generici'
